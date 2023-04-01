96af15ca8df6ea298615f05ecd2bb7ad29e5b2e4
code-vine <caldwell.vincent@outlook.com>
2023-04-01 13:31:08 -0500

    changes app to react component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <audio controls> <source src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" type="audio/mp3" /> </audio>
       </div>
     );
   }
